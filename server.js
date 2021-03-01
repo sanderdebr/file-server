37d153c03f98dc80917d5488a03f5746c502b024
sanderdebr <sander606@gmail.com>
2021-03-01 19:30:04 +0100

    adding files through HTTP requests

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const { createServer } = require("http");
-const { parse } = require("url");
-const { resolve, sep } = require("path");
 const { createReadStream, createWriteStream, mkdir, fstat } = require("fs");
 const { stat, readdir, rmdir, unlink } = require("fs").promises;
 const mime = require("mime");
+const { notAllowed, urlPath, pipeStream } = require("./helpers");
 
 // Methods object holds methods
 const methods = Object.create(null);
 
-// Get current directory
-const baseDirectory = process.cwd();
+const WEBSITE_ROOT = "/website";
 
 // GET files or directories
 methods.GET = async function (request) {
+  if (request.url === "/") {
+    console.log("index");
+    return {
+      body: createReadStream(process.cwd() + WEBSITE_ROOT + "/index.html"),
+      type: "text/html",
+    };
+  }
+
+  // Return files or directories
   let path = urlPath(request.url);
   let stats;
   try {
@@ -87,30 +94,3 @@ createServer((request, response) => {
       else response.end(body);
     });
 }).listen(8000);
-
-async function notAllowed(request) {
-  return {
-    status: 405,
-    body: `Method ${request.method} not allowed.`,
-  };
-}
-
-// Transform request URL to path
-function urlPath(url) {
-  let { pathname } = parse(url);
-  let path = resolve(decodeURIComponent(pathname).slice(1));
-  if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
-    throw { status: 403, body: "Forbidden" };
-  }
-  return path;
-}
-
-// Wrapper that creates promise around outcome of calling pipe(), since pipe() does not return a promise
-function pipeStream(from, to) {
-  return new Promise((resolve, reject) => {
-    from.on("error", reject); // request falls in error
-    to.on("error", reject); // createWriteStream falls in error
-    to.on("finish", resolve); // createWriteStream is ready
-    from.pipe(to); // use pipe to move data from readable to writable stream
-  });
-}
